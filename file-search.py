b3ad6356e00b2efdf372347c4d78bfae0ef45c90
ericj79 <ericj79@gmail.com>
2017-11-18 09:04:32 -0500

    Files are searched

--- file-search.py
+++ file-search.py
@@ -1,13 +1,30 @@
 #! python
 import argparse
-import os
+from os import listdir, getcwd
+from os.path import isfile, join, abspath
 
 
 parser = argparse.ArgumentParser(description="Look for string in files")
 parser.add_argument('search', help="The string to search for")
-parser.add_argument('--dir', dest="directory", default=os.getcwd(), help="Optional directory to search. Default: current directory")
+parser.add_argument('--dir', dest="directory", default=getcwd(), help="Optional directory to search. Default: current directory")
 
 args = parser.parse_args()
 
-print(args.search) 
-print(args.directory)
+needle = args.search
+path = abspath(args.directory)
+print "\nSearching all files in " + path + " for the '" + needle + "':\n"
+
+allFiles = [filename for filename in listdir(path) if isfile(join(path, filename))]
+
+fileMatches = 0
+contentMatches = 0
+for filename in allFiles:
+    if needle in filename:
+        print "File Name match: " + filename
+        fileMatches = fileMatches + 1
+    if needle in open(join(path, filename)).read():
+        print "Contents match: " + filename
+        contentMatches = contentMatches + 1
+
+print "\n{} file names matched".format(fileMatches)
+print "{} file contents matched".format(contentMatches)
