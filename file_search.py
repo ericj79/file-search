7570d1bb031c77c7629609592ab252ad88288500
ericj79 <ericj79@gmail.com>
2018-01-13 10:07:40 -0500

    cleaned up all pylint errors

--- file_search.py
+++ file_search.py
@@ -1,11 +1,12 @@
 #! python
+"""Simple script to find files in a directory that contain a string in the file name or contents."""
 
 # import needed libraries
 import argparse
 from os import listdir, getcwd
 from os.path import isfile, join, abspath
 
-
+# Parse command line arguments. we will need a string to search for and
 PARSER = argparse.ArgumentParser(description="Look for string in files")
 PARSER.add_argument('search', help="The string to search for")
 PARSER.add_argument('--dir', dest="directory", default=getcwd(),
@@ -13,22 +14,22 @@ PARSER.add_argument('--dir', dest="directory", default=getcwd(),
 
 ARGS = PARSER.parse_args()
 
-needle = ARGS.search
-path = abspath(ARGS.directory)
-print "\nSearching all files in " + path + " for the '" + needle + "':\n"
+NEEDLE = ARGS.search
+PATH = abspath(ARGS.directory)
+print "\nSearching all files in " + PATH + " for the '" + NEEDLE + "':\n"
 
-allFiles = [filename for filename in listdir(
-    path) if isfile(join(path, filename))]
+ALL_FILES = [filename for filename in listdir(
+    PATH) if isfile(join(PATH, filename))]
 
-fileMatches = 0
-contentMatches = 0
-for filename in allFiles:
-    if needle in filename:
+FILE_MATCHES = 0
+CONTENT_MATCHES = 0
+for filename in ALL_FILES:
+    if NEEDLE in filename:
         print "File Name match: " + filename
-        fileMatches = fileMatches + 1
-    if needle in open(join(path, filename)).read():
+        FILE_MATCHES = FILE_MATCHES + 1
+    if NEEDLE in open(join(PATH, filename)).read():
         print "Contents match: " + filename
-        contentMatches = contentMatches + 1
+        CONTENT_MATCHES = CONTENT_MATCHES + 1
 
-print "\n{} file names matched".format(fileMatches)
-print "{} file contents matched".format(contentMatches)
+print "\n{} file names matched".format(FILE_MATCHES)
+print "{} file contents matched".format(CONTENT_MATCHES)
