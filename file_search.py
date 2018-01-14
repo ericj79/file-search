e2f56d5a8b2bdcea7c0ecbc635633a93b52b324f
ericj79 <ericj79@gmail.com>
2018-01-14 09:05:28 -0500

    Added comments

--- file_search.py
+++ file_search.py
@@ -6,7 +6,8 @@ import argparse
 from os import listdir, getcwd
 from os.path import isfile, join, abspath
 
-# Parse command line arguments. we will need a string to search for and
+# Parse command line arguments using arparse.ArgumentParser.
+# We will need a string to search for and an optional directory to search in
 PARSER = argparse.ArgumentParser(description="Look for string in files")
 PARSER.add_argument('search', help="The string to search for")
 PARSER.add_argument('--dir', dest="directory", default=getcwd(),
@@ -14,22 +15,29 @@ PARSER.add_argument('--dir', dest="directory", default=getcwd(),
 
 ARGS = PARSER.parse_args()
 
+# Get search and directory arguements
 NEEDLE = ARGS.search
 PATH = abspath(ARGS.directory)
-print "\nSearching all files in " + PATH + " for the '" + NEEDLE + "':\n"
+print("\nSearching all files in " + PATH + " for the '" + NEEDLE + "':\n")
 
+# Get a list of all files in the directory
 ALL_FILES = [filename for filename in listdir(
     PATH) if isfile(join(PATH, filename))]
 
+# initialize our counter variables for file name matches and file content matches
 FILE_MATCHES = 0
 CONTENT_MATCHES = 0
+
+# loop through all files. Check if search string is in file name and file content
+# if a match is found. Increase the correct counter and print the file name
 for filename in ALL_FILES:
     if NEEDLE in filename:
-        print "File Name match: " + filename
+        print("File Name match: " + filename)
         FILE_MATCHES = FILE_MATCHES + 1
     if NEEDLE in open(join(PATH, filename)).read():
-        print "Contents match: " + filename
+        print("Contents match: " + filename)
         CONTENT_MATCHES = CONTENT_MATCHES + 1
 
-print "\n{} file names matched".format(FILE_MATCHES)
-print "{} file contents matched".format(CONTENT_MATCHES)
+# print out the results
+print("\n{} file names matched".format(FILE_MATCHES))
+print("{} file contents matched".format(CONTENT_MATCHES))
